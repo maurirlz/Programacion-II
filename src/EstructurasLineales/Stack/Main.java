16076f92a33a2f73598b29ae28a977dcbb072a56
maaush <mauribenitez95@gmail.com>
2019-11-17 03:26:25 -0300

    Finish Stack class and add a test for it.

---
+++ src/EstructurasLineales/Stack/Main.java
@@ -0,0 +1,22 @@
+package EstructurasLineales.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(50);
+        stack.push(100);
+        stack.push(150);
+        stack.push(200); // head
+
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+
+        stack.pop();
+        System.out.println(stack.peek());
+
+
+    }
+}
---
+++ src/EstructurasLineales/Stack/Stack.java
@@ -0,0 +1,38 @@
+package EstructurasLineales.Stack;
+
+import java.util.NoSuchElementException;
+
+public class Stack<T> {
+
+   Node<T> head;
+
+   public void push(T stuff) {
+
+       head = new Node<>(stuff, this.head);
+   }
+
+   public T pop() throws NoSuchElementException {
+       if (head == null) {
+
+           throw new NoSuchElementException();
+       }
+
+       T aux = head.data;
+       head = head.previous;
+
+       return aux;
+   }
+
+   public boolean isEmpty() {
+       return head == null;
+   }
+
+   public T peek() {
+       if (head == null) {
+
+           return null;
+       }
+
+       return head.data;
+   }
+}
